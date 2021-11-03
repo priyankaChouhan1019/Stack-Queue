a596fa6607aa90116e60ea7489ca6a2997284379
webPriya <priyachouhan1019@gmail.com>
2021-11-03 09:18:50 -0700

    create stack list

---
+++ src/main/java/com/bdlz/StackQueue/Operation.java
@@ -0,0 +1,16 @@
+package com.bdlz.StackQueue;
+
+//import java.util.Stack;
+
+public class Operation {
+    public Stack addElement() {
+        Stack stackList =new Stack();
+        stackList.push(70);
+        stackList.push(30);
+        stackList.push(56);
+
+        stackList.display();
+        return stackList;
+
+    }
+}
---
+++ src/main/java/com/bdlz/StackQueue/StackMain.java
@@ -0,0 +1,18 @@
+package com.bdlz.StackQueue;
+
+import java.util.Scanner;
+
+public class StackMain {
+    public static void main(String[] args) {
+        System.out.println("Welcome to the stack program");
+        Operation operation = new Operation();
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Press '1' to add element at the start  ");
+        switch (scanner.nextInt()) {
+            case 1:
+                operation.addElement();
+                break;
+        }
+        scanner.close();
+    }
+}
